5cc3f31aa171747f772c9da073572052d5855a05
tails5555 <tails5555@naver.com>
2018-05-09 22:56:00 +0900

    Update CompanyService.java findOne, update 함수(수정 가능성 있음) 추가.

--- src/main/java/net/kang/main/controller/CompanyController.java
+++ src/main/java/net/kang/main/controller/CompanyController.java
@@ -3,12 +3,10 @@ package net.kang.main.controller;
 import net.kang.main.domain.Company;
 import net.kang.main.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @CrossOrigin
@@ -18,5 +16,9 @@ public class CompanyController {
     @GetMapping("all_company")
     public Flux<Company> allCompany(){
         return companyService.findAll();
+    }
+    @GetMapping("one_company/{id}")
+    public Mono<Company> oneCompany(@PathVariable("id") String id){
+        return companyService.findOne(id);
     }
 }
--- src/main/java/net/kang/main/service/CompanyService.java
+++ src/main/java/net/kang/main/service/CompanyService.java
@@ -5,11 +5,25 @@ import net.kang.main.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class CompanyService {
     @Autowired CompanyRepository companyRepository;
     public Flux<Company> findAll(){
         return companyRepository.findAll();
+    }
+    public Mono<Company> findOne(String id){
+        return companyRepository.findById(id);
+    }
+    public void update(Company company){
+        companyRepository.findById(company.getId())
+            .map(c -> {
+                c.setName(company.getName());
+                c.setFoundDate(company.getFoundDate());
+                c.setAddress(company.getAddress());
+                return c;
+            })
+            .flatMap(c -> companyRepository.save(c));
     }
 }
